66f660aef3dfa37c6cf3075c09166d71411f01fc
Niraj Kaushal <nirajkaushal007@gmail.com>
2023-05-30 21:27:14 +0530

    feat(app) - add tailwind + cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="h-screen w-screen bg-gray-100 flex flex-col gap-2 justify-center items-center p-4">
+      <h1 className="text-2xl text-center">
+        Capacitorjs + React + Tailwind Template
+      </h1>
+      <h3 className="text-center">
+        A template to get started with capacitor js, react and tailwind to
+        create mobile app
+      </h3>
+    </main>
   )
 }
 

ed56cda6615e1059e4850856a2901e7b1224104c
Niraj Kaushal <nirajkaushal007@gmail.com>
2023-06-01 18:09:15 +0530

    feat(app) - add file based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-function App() {
-  return (
-    <main className="h-screen w-screen bg-gray-100 flex flex-col gap-2 justify-center items-center p-4">
-      <h1 className="text-2xl text-center">
-        Capacitorjs + React + Tailwind Template
-      </h1>
-      <h3 className="text-center">
-        A template to get started with capacitor js, react and tailwind to
-        create mobile app
-      </h3>
-    </main>
-  )
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// File Based Routing: https://dev.to/franciscomendes10866/file-based-routing-using-vite-and-react-router-3fdo
+const pages: any = import.meta.glob("./pages/**/*.tsx", { eager: true })
+
+const routes = []
+
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1]
+  if (!fileName) {
+    continue
+  }
+
+  const normalizedPathName = fileName.includes("$")
+    ? fileName.replace("$", ":")
+    : fileName.replace(/\/index/, "")
+
+  routes.push({
+    path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
+    Element: pages[path].default,
+    ErrorBoundary: pages[path]?.ErrorBoundary,
+  })
+}
+
+const router = createBrowserRouter(
+  routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+    ...rest,
+    element: <Element />,
+    ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+  }))
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
